5f321bfb8b551582c223aa1392fab94fcd1e94ae
takeshi <20181153@lamolina.edu.pe>
2019-11-24 13:39:42 -0500

    mi primer commit

---
+++ Git.py
@@ -0,0 +1,17 @@
+Es un sistema de control de versiones(VCS) para Github.
+Basic commands:
+    git init , empezar a trabjar con git
+    git add <file> ,
+    git status , para ver en que estado estan mis archivos
+    git commit 
+    git pull
+    git clone
+
+Estados :
+    
+-working directory, donde trabajas con tus archivos
+
+-staging area,
+
+-repository,
+
