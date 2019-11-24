6830a2d54babbd53646d840fd2f9c6f640155e6d
takeshi <20181153@lamolina.edu.pe>
2019-11-24 14:04:15 -0500

    segundo commit

--- Git.py
+++ Git.py
@@ -1,12 +1,16 @@
 Es un sistema de control de versiones(VCS) para Github.
 Basic commands:
     git init , empezar a trabjar con git
-    git add <file> ,
+    git add <file> , para agregar un archivo al stating area
     git status , para ver en que estado estan mis archivos
-    git commit 
-    git pull
-    git clone
-
+    git commit , para crear un primer punto de control de nuestro codigo
+    git pull,
+    git clone,
+    git config --global user.email "20181153@lamolina.edu.pe"
+    git config --global user.name "takeshi"
+    git log, para ver todos los commits que hemos creado
+    git diff, para ver las diferencias hechas en los archivos
+    
 Estados :
     
 -working directory, donde trabajas con tus archivos
@@ -15,3 +19,6 @@ Estados :
 
 -repository,
 
+Vin Es el editor de código de git desde la consola, ahí escribes un comentario para la nueva versión que estés versionando (si no te deja escribir presiona a letra i).
+Cuando termines presiona esc y luego :wq (write & quit)pasa salir.
+ 
